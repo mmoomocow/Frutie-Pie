245a165cba45c378bbdaef0d7d4f0023814f1163
JerryFFFFFF <88009027+JerryFFFFFF@users.noreply.github.com>
2022-07-22 20:57:55 +1200

    Left right movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+	Rigidbody2D rb;
+	BoxCollider2D bc;
+	[SerializeField] public float speed = 10f;
+	[SerializeField] public float jumpForce = 10f;
 	void Start()
 	{
-		
+		rb = GetComponent<Rigidbody2D>();
+		bc = GetComponent<BoxCollider2D>();
 	}
 
 	void Update()
 	{
-		
+		float x = Input.GetAxis("Horizontal");
+		Vector2 movement = new Vector2(x, 0);
+		rb.velocity = movement * speed;
 	}
 }
